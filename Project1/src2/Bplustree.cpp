6d1bf042dab11414d57642de8fb9163e498f3847
Leonard249 <Leonarddokj@gmail.com>
2024-10-02 16:21:23 +0800

    smd

--- Project1/src2/Bplustree.cpp
+++ Project1/src2/Bplustree.cpp
@@ -3,15 +3,9 @@
 
 #include <iostream>
 #include <string>
-<<<<<<< Updated upstream
-#include <vector>    //Used for initialising keys and pointers
-#include <algorithm> //Used for sorting arrays
-#include <record.h>
-=======
 #include <vector>
 #include <algorithm>
 #include <fstream> // For file handling
->>>>>>> Stashed changes
 
 using namespace std;
 
@@ -45,21 +39,12 @@ struct Record
 class Node
 {
 public:
-<<<<<<< Updated upstream
-    bool isLeaf;                 // Determined whether node is leaf
-    int currkeysize;             // Current number of keys
-    int maxkeysize;              // Max number of keys in a node
-    vector<int> keys;            // Array of Keys
-    vector<Node *> pointers;     // Array of Pointers
-    vector<record *> recordpointer; // RecordPointers
-=======
     bool isLeaf;                     // True if the node is a leaf, false if internal
     int currkeysize;                 // Current number of keys
     int maxkeysize;                  // Max number of keys in a node
     vector<int> keys;                // Array of keys
     vector<Node *> pointers;         // For internal nodes, pointers to children
     vector<Record *> recordPointers; // For leaf nodes, pointers to records
->>>>>>> Stashed changes
     Node *nextnode;
     int minkeysize;
     int minleafkeysize;
@@ -69,13 +54,6 @@ public:
     {
         this->isLeaf = isLeaf;
         this->maxkeysize = maxkeysize;
-<<<<<<< Updated upstream
-        this->currkeysize = 0;                       // Initially, no keys are in the node
-        this->minleafkeysize = (maxkeysize + 1) / 2; // minkeysize for leaf node
-        this->minkeysize = maxkeysize / 2;           // minkeysize for internal node
-        keys.resize(maxkeysize);                     // Pre-allocate space for the keys
-        pointers.resize(maxkeysize + 1, nullptr);    // Pre-allocate for pointers (maxkeysize + 1)
-=======
         this->currkeysize = 0;
         keys.resize(maxkeysize);
         if (isLeaf)
@@ -86,7 +64,6 @@ public:
         {
             pointers.resize(maxkeysize + 1, nullptr); // For internal nodes
         }
->>>>>>> Stashed changes
     }
 };
 
@@ -96,120 +73,6 @@ public:
     Node *root;
     int maxkeysize; // Maximum keys in a node
 
-<<<<<<< Updated upstream
-    // Constructor
-    Bplustree();
-
-    // Functions
-    Node *findNode(int);
-    void deleteKey(int);
-    Node *searchKey();
-    void insert(int);
-    bool BorrowNode(Node *, Node *);
-    void MergeNode(Node *, Node *);
-};
-
-Bplustree::Bplustree(/* args */)
-{
-}
-
-Bplustree::~Bplustree()
-{
-}
-
-Node *Bplustree::findNode(int key)
-{
-    Node *CurrNode = root;
-    while (!CurrNode->isLeaf) // Traverse to leaf node
-    {
-        auto UpperBound = upper_bound(CurrNode->keys.begin(), CurrNode->keys.end(), key);
-        int index = UpperBound - CurrNode->keys.begin();
-        CurrNode = CurrNode->pointers[index];
-    }
-
-    return CurrNode; // return the leaf node
-};
-
-void Bplustree::deleteKey(int key)
-{
-    Node *LeafNode = this->findNode(key);
-    Node *ParentNode = this.FindParent(this.root, LeafNode);
-
-    // Delete from node
-    bool found = false;
-    for (int i = 0; i < LeafNode->currkeysize; i++)
-    {
-        if (LeafNode->keys[i] == key)
-        {
-            LeafNode->keys.erase(i);
-            LeafNode->recordpointer.erase(i);
-            LeafNode->currkeysize = LeafNode->keys.size();
-            found = true;
-            break;
-        }
-    }
-
-    if (found)
-    {
-        bool success;
-        // check for violation
-        if (LeafNode->currkeysize < LeafNode->minleafkeysize)
-        {
-            success = this.BorrowNode(ParentNode, LeafNode);
-
-            if (!success)
-            {
-                this.MergeNode(ParentNode, LeafNode);
-            }
-        }
-    }
-    else
-    {
-        cout << "Key not found no key to delete" << endl;
-    }
-
-    // Can borrow from left or right
-};
-
-void Bplustree::BorrowNode(Node *ParentNode, Node *LeafNode)
-{
-    // CheckIfLeft&RightExist
-
-    Node *RightNode = LeafNode->nextnode;
-    if (RightNode->currkeysize > RightNode->minleafkeysize)
-    {
-        int borrowedkey = RightNode->keys[0];
-        int *borrowedPointer = RightNode->recordpointer[0];
-    }
-    else if
-    {
-        // Check Left
-    }
-}
-
-void Bplustree::MergeNode(Node *ParentNode, Node *LeadNode)
-{
-
-}
-
-Node *Bplustree::findParent(Node *cursor, Node *child)
-{
-    if (cursor->isLeaf || cursor->pointers[0]->isLeaf)
-        return nullptr;
-    for (int i = 0; i < cursor->currkeysize + 1; i++)
-    {
-        if (cursor->pointers[i] == child)
-            return cursor;
-        else
-        {
-            Node *parent = findParent(cursor->pointers[i], child);
-            if (parent != nullptr)
-                return parent;
-        }
-    }
-    return nullptr;
-}
-=======
     Bplustree(int maxkeysize) : maxkeysize(maxkeysize)
     {
         root = nullptr;
@@ -390,4 +253,3 @@ Node *Bplustree::findParent(Node *cursor, Node *child)
     {
     }
 };
->>>>>>> Stashed changes
