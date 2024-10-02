584d992ff3ebebe1b0f26eb4efbe61bf93a5ffd1
Evauts <chuanyoutan@gmail.com>
2024-10-02 10:59:17 +0800

    Node

--- Project1/src2/Bplustree.cpp
+++ Project1/src2/Bplustree.cpp
@@ -5,3 +5,14 @@
 
 using namespace std;
 
+class Node
+{
+    public:
+        bool isLeaf;                    //Determined whether node is leaf
+        int currkeysize;                //Current number of keys               
+        int maxkeysize;                    //Max number of keys in a node    
+        vector<int> keys;               //Array of Keys 
+        vector<Node*> pointers;         //Array of Pointers
+       
+};
+
