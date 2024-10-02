df2af9223a65bd80c6e3393d4b0bfb93da929bd0
Evauts <chuanyoutan@gmail.com>
2024-10-02 11:01:13 +0800

    Update Bplustree.cpp

--- Project1/src2/Bplustree.cpp
+++ Project1/src2/Bplustree.cpp
@@ -13,6 +13,14 @@ class Node
         int maxkeysize;                    //Max number of keys in a node    
         vector<int> keys;               //Array of Keys 
         vector<Node*> pointers;         //Array of Pointers
-       
+
+
+    Node(int maxkeysize, bool isLeaf) {
+        this->isLeaf = isLeaf;
+        this->maxkeysize = maxkeysize;
+        this->currkeysize = 0;      // Initially, no keys are in the node
+        keys.resize(maxkeysize);    // Pre-allocate space for the keys
+        pointers.resize(maxkeysize + 1, nullptr); // Pre-allocate for pointers (maxkeysize + 1)
+    }
 };
 
